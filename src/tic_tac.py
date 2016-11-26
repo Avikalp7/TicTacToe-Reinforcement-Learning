2820a9b5ff5794008839d13867aadc95262231ce
Avikalp7 <avikalp22@gmail.com>
2016-11-26 21:50:15 +0530

    Document the code

--- src/tic_tac.py
+++ src/tic_tac.py
@@ -1,15 +1,15 @@
 import copy
-from random import randint 
 import pickle
-from prettytable import PrettyTable
 import prettytable
 import time
+from prettytable import PrettyTable
 
 
 def decimal_to_three_base(num):
+	""" Convert a given decimal number to base 3 number and return it in the format of a state [[, , ], [, , ], [, , ]]"""
 	vars = [0]*9
 	three_pows = [1,3,9,27,81,243,729,2187,6561]
-	t= num
+	t = num
 	for j in xrange(8, -1, -1):
 		vars[j] = t/three_pows[j]
 		t = t%three_pows[j]
@@ -18,6 +18,7 @@ def decimal_to_three_base(num):
     
 
 def categorize(state):
+	""" Given a state, categorize it as winning(good)=0/ losing(bad)=1/ tie=2 or incomplete game=3 state"""
 	if (state[0][0] == 2 and state[0][1] == 2 and state[0][2] == 2) or (
 		state[1][0] == 2 and state[1][1] == 2 and state[1][2] == 2) or (
 		state[2][0] == 2 and state[2][1] == 2 and state[2][2] == 2) or (
@@ -36,26 +37,32 @@ def categorize(state):
 		state[0][0] == 0 and state[1][1] == 0 and state[2][2] == 0) or (
 		state[0][2] == 0 and state[1][1] == 0 and state[2][0] == 0):
 		return 1 # GAME LOST
-	exp = True
+	
+	tie_exp = True
 	for i in range(0,3):
-		if not exp:
+		if not tie_exp:
 			break
 		for j in range(0,3):
 			if state[i][j] == 1:
-				exp = False
+				tie_exp = False
 				break
-	if exp:
+	if tie_exp:
 		return 2 # TIE
 	else:
 		return 3 # GAME ON
 
-def set_initial_values(state_num, good_states, bad_states):
+
+def set_initial_values(state_num, good_states, bad_states, tie_states):
+	""" 
+	Sets the initial values for leaarning rate, intitial V* values for good/bad/tie final states 
+	It also buids the sets - good/bad/tie sets, each containing all possible sets of their respective scenario
+	"""
 	global learning_rate
 	learning_rate = 0.5
 	good_value = 1000
 	bad_value = -1000
 	tie_value = 100
-	for i in xrange(0, 19683):
+	for i in xrange(0, 19683):						# Considering all 3^9 states
 		state = decimal_to_three_base(i)
 		category = categorize(state)
 		if category == 0:
@@ -68,8 +75,9 @@ def set_initial_values(state_num, good_states, bad_states):
 			tie_states.append(state)
 			state_num[i] = tie_value
 
-def vaild_goto_states(current_state):
-	if current_state in good_states or current_state in bad_states:
+def vaild_goto_states(current_state, good_states, bad_states):
+	""" Compute valid states that the machine can go to from the current state and returns them as a list """
+	if current_state in good_states or current_state in bad_states:				# If final state reached, return to init
 		return [init_state]
 	goto_states = []
 	for i in range(0,3):
@@ -82,9 +90,11 @@ def vaild_goto_states(current_state):
 	return goto_states
 
 def is_invalid_choice(i, j, current_state):
+	""" Given the choice of state (i, j) by user, check the validity of the move """
 	return i < 0 or j < 0 or i > 2 or j > 2 or current_state[i][j] != 1
 
 def convert_state_to_decimal(state):
+	""" Converts the given state of game (base 3) to decimal value """
 	decimal_value = 0
 	three_pow = 6561
 	for k in range(0, 9):
@@ -94,126 +104,140 @@ def convert_state_to_decimal(state):
 		three_pow /= 3
 	return decimal_value 
 
-learning_rate = 0.0
-try:
-	state_num_temp = pickle.load(open('save_vstar.p', 'rb'))
-	choice = raw_input('Load previous progress - Y\~Y : ')
-	if choice == 'Y':
-		state_num = state_num_temp
-	else:
-		state_num = [0]*19683
-except:
-	state_num = [0]*19683
 
-good_states = []
-bad_states = []
-tie_states = []
-state = [[1,1,1], [1,1,1], [1,1,1]]
+def main():
+	learning_rate = 0.0
+	try:
+		state_num_temp = pickle.load(open('save_vstar.p', 'rb'))		# Check if any progress exists
+		choice = raw_input('Load previous progress - Y\~Y : ')	
+		if choice == 'Y':
+			state_num = state_num_temp
+		else:
+			state_num = [0]*19683
+	except:
+		state_num = [0]*19683											# No progress => zero initialization
 
-set_initial_values(state_num, good_states, bad_states)
-init_state = [[1,1,1],[1,1,1],[1,1,1]]
-current_state = copy.deepcopy(init_state)
-mapping = {0:'O', 1: ' ', 2: 'X'}
-iter1 = True
-while(True):
-	pickle.dump(state_num, open('save_vstar.p', 'wb'))
-	if iter1:
-		print 'Current State of the Game :'
+	good_states = []
+	bad_states = []
+	tie_states = []
+	set_initial_values(state_num, good_states, bad_states, tie_states)
+	init_state = [[1,1,1],[1,1,1],[1,1,1]]
+	current_state = copy.deepcopy(init_state)
+	
+	mapping = {0:'O', 1: ' ', 2: 'X'}									# MAPPING : O -> 0, _ -> 1, X -> 2
+	
+	iter1 = True														# 1st iteration Bool												
+	while(True):
+		pickle.dump(state_num, open('save_vstar.p', 'wb'))			# Save the progress thus far
+		if iter1:
+			print 'Current State of the Game :'
+			t = PrettyTable(header = False, hrules = prettytable.ALL)
+			t.add_row([mapping[current_state[0][0]], mapping[current_state[0][1]], mapping[current_state[0][2]]])
+			t.add_row([mapping[current_state[1][0]], mapping[current_state[1][1]], mapping[current_state[1][2]]])
+			t.add_row([mapping[current_state[2][0]], mapping[current_state[2][1]], mapping[current_state[2][2]]])
+			print t
+			# iter1 is set to false down below
+
+		print 'Machine Thinking...'
+		time.sleep(1)
+
+		goto_states = vaild_goto_states(current_state, good_states, bad_states)
+		n = len(goto_states)
+		max_vstar = -10000
+		for state in goto_states:
+			temp = state_num[convert_state_to_decimal(state)]
+			if temp > max_vstar:
+				max_vstar = temp
+				try:
+					goto_state[:] = []
+				except:
+					pass
+				goto_state = copy.deepcopy(state)
+
+		if goto_state == init_state:
+			current_state = copy.deepcopy(init_state)
+			print 'Returning to initial state'
+			continue
+		
+		temp = learning_rate*state_num[convert_state_to_decimal(goto_state)]
+		
+		state_num[convert_state_to_decimal(current_state)] = temp
+		
+		current_state[:] = []
+		current_state = copy.deepcopy(goto_state)
+
+		t.clear()
 		t = PrettyTable(header = False, hrules = prettytable.ALL)
 		t.add_row([mapping[current_state[0][0]], mapping[current_state[0][1]], mapping[current_state[0][2]]])
 		t.add_row([mapping[current_state[1][0]], mapping[current_state[1][1]], mapping[current_state[1][2]]])
 		t.add_row([mapping[current_state[2][0]], mapping[current_state[2][1]], mapping[current_state[2][2]]])
 		print t
 
-	print 'Machine Thinking...'
-	time.sleep(1)
+		if current_state in good_states:
+			print 'The machine has won!\nRise of the planet of the Duex Machina!'
+			current_state[:] = []
+			current_state = copy.deepcopy(init_state)
+			temp = raw_input('Press enter to continue')
+			continue
 
-	goto_states = vaild_goto_states(current_state)
-	n = len(goto_states)
-	max_vstar = -10000
-	for state in goto_states:
-		temp = state_num[convert_state_to_decimal(state)]
-		if temp > max_vstar:
-			max_vstar = temp
+		if current_state in bad_states:
+			print 'User has won'
+			current_state[:] = []
+			current_state = copy.deepcopy(init_state)
+			temp = raw_input('Press enter to continue')
+			continue
+
+		if current_state in tie_states:
+			print 'Its a TIE!'
+			current_state[:] = []
+			current_state = copy.deepcopy(init_state)
+			temp = raw_input('Press enter to continue')
+			continue
+
+		print 'YOUR MOVE!'
+		i = -1
+		j = -1
+		while is_invalid_choice(i, j, current_state):
+			print 'Input an available cell number',
+			if iter1:
+				print '(Eg. format -> 0 2 ) : ',
+				iter1 = False
+			else:
+				print ': ',
+			str_arr = raw_input().split(' ') 
 			try:
-				goto_state[:] = []
-			except:
-				pass
-			goto_state = copy.deepcopy(state)
+				i,j = [int(num) for num in str_arr]
+			except ValueError:
+				i, j = [-1, -1]
+				continue
 
-	if goto_state == init_state:
-		current_state = copy.deepcopy(init_state)
-		print 'Returning to initial state'
-		continue
-	
-	temp = learning_rate*state_num[convert_state_to_decimal(goto_state)]
-	
-	state_num[convert_state_to_decimal(current_state)] = temp
-	
-	current_state[:] = []
-	current_state = copy.deepcopy(goto_state)
-	# print 'Now, machine has made a move, current state has become :',
-	# print current_state
-	# print state_num[convert_state_to_decimal(goto_state)]
-	t.clear()
-	t = PrettyTable(header = False, hrules = prettytable.ALL)
-	t.add_row([mapping[current_state[0][0]], mapping[current_state[0][1]], mapping[current_state[0][2]]])
-	t.add_row([mapping[current_state[1][0]], mapping[current_state[1][1]], mapping[current_state[1][2]]])
-	t.add_row([mapping[current_state[2][0]], mapping[current_state[2][1]], mapping[current_state[2][2]]])
-	print t
+		prev_state = copy.deepcopy(current_state)
+		current_state[i][j] = 0
 
-	if current_state in good_states:
-		print 'The machine has won!\nRise of the planet of the Duex Machina!'
-		current_state[:] = []
-		current_state = copy.deepcopy(init_state)
-		t = raw_input('Press enter to continue')
-		continue
+		t.clear()
+		t = PrettyTable(header = False, hrules = prettytable.ALL)
+		t.add_row([mapping[current_state[0][0]], mapping[current_state[0][1]], mapping[current_state[0][2]]])
+		t.add_row([mapping[current_state[1][0]], mapping[current_state[1][1]], mapping[current_state[1][2]]])
+		t.add_row([mapping[current_state[2][0]], mapping[current_state[2][1]], mapping[current_state[2][2]]])
+		print t
+		t.clear()
+		temp = learning_rate*state_num[convert_state_to_decimal(current_state)]
+		state_num[convert_state_to_decimal(prev_state)] = temp
 
-	if current_state in bad_states:
-		print 'User has won'
-		current_state[:] = []
-		current_state = copy.deepcopy(init_state)
-		t = raw_input('Press enter to continue')
-		continue
+		if current_state in bad_states:
+			print 'User has won!'
+			current_state[:] = []
+			current_state = copy.deepcopy(init_state)
+			temp = raw_input('Press enter to continue')
+			continue
+		
+		if current_state in tie_states:
+			print 'Its a TIE!'
+			current_state[:] = []
+			current_state = copy.deepcopy(init_state)
+			temp = raw_input('Press enter to continue')
+			continue
 
-	if current_state in tie_states:
-		print 'Its a TIE!'
-		current_state[:] = []
-		current_state = copy.deepcopy(init_state)
-		t = raw_input('Press enter to continue')
-		continue
 
-	print 'YOUR MOVE!'
-	i = -1
-	j = -1
-	while is_invalid_choice(i, j, current_state):
-		str_arr = raw_input('Input an available cell number (Eg. format -> 0 2 ) : ').split(' ') 
-		i,j = [int(num) for num in str_arr]
-
-	prev_state = copy.deepcopy(current_state)
-	current_state[i][j] = 0
-	# print 'New Current State after user input is = ',
-	# print current_state
-	t.clear()
-	t = PrettyTable(header = False, hrules = prettytable.ALL)
-	t.add_row([mapping[current_state[0][0]], mapping[current_state[0][1]], mapping[current_state[0][2]]])
-	t.add_row([mapping[current_state[1][0]], mapping[current_state[1][1]], mapping[current_state[1][2]]])
-	t.add_row([mapping[current_state[2][0]], mapping[current_state[2][1]], mapping[current_state[2][2]]])
-	print t
-	t.clear()
-	temp = learning_rate*state_num[convert_state_to_decimal(current_state)]
-	state_num[convert_state_to_decimal(prev_state)] = temp
-
-	if current_state in bad_states:
-		print 'User has won!'
-		current_state[:] = []
-		current_state = copy.deepcopy(init_state)
-		t = raw_input('Press enter to continue')
-		continue
-	
-	if current_state in tie_states:
-		print 'Its a TIE!'
-		current_state[:] = []
-		current_state = copy.deepcopy(init_state)
-		t = raw_input('Press enter to continue')
-		continue
+if __name__ == '__main__':
+	main()
